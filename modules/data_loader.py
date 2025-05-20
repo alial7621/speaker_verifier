7bd564380d7942a6cddf8d7128104ed2e9dba2b3
alial7621 <a_lashini@yahoo.com>
2025-05-20 22:37:26 +0330

    feat(data_loader): add valid dataset class

--- modules/data_loader.py
+++ modules/data_loader.py
@@ -120,12 +120,12 @@ class TrainDataset(Dataset):
         if pos_or_neg:
             speaker_id = random.choice(list(self.pos_speaker_to_files.keys()))
             file1, file2 = random.sample(self.pos_speaker_to_files[speaker_id], 2)
-            label = torch.tensor(1, dtype=torch.long)
+            label = torch.tensor(1, dtype=torch.float)
         else:
             speakers_id = random.sample(list(self.all_speakers_to_files.keys()), 2)
             file1 = random.choice(self.all_speakers_to_files[speakers_id[0]])
             file2 = random.choice(self.all_speakers_to_files[speakers_id[1]])
-            label = torch.tensor(0, dtype=torch.long)
+            label = torch.tensor(0, dtype=torch.float)
         return file1, file2, label
     
     def _get_triplet_pair(self):
@@ -135,6 +135,57 @@ class TrainDataset(Dataset):
         anchor, positive = random.sample(self.pos_speaker_to_files[anchor_id], 2)
         negative = random.choice(self.all_speakers_to_files[negative_id])
         return anchor, positive, negative
+    
+
+class ValidDataset(Dataset):
+    def __init__(self, dataset_path, sample_rate=16000, duration=3, vad=False):
+        """   
+        Parameters
+        ----------
+        dataset_path : str
+            path to a CSV file containing columns 'audio_path_1', 'audio_path_2', 'label'
+        sample_rate : int, optional
+            sample rate of audio files
+        duration : int, optional
+            duration of audio files in seconds
+        vad : bool, optional
+            whether to use voice activity detection on audio files
+        """
+        self.dataset_df = pd.read_csv(dataset_path)
+        self.sample_rate = sample_rate
+        self.fixed_length = int(sample_rate * duration)
+        self.vad = vad
+
+    def __len__(self):
+        return len(self.dataset_df)
+    
+    def _preprocess(self, path):
+        waveform, sr = torchaudio.load(path)
+        if sr != self.sample_rate:
+            resampler = transforms.Resample(orig_freq=sr, new_freq=self.sample_rate)
+            waveform = resampler(waveform)
+
+        if self.vad:
+            waveform, _ = torchaudio.sox_effects.apply_effects_tensor(
+                waveform, self.sample_rate, [['vad', '-t', '3'], ['rate', str(self.sample_rate)]])
+        else:
+            waveform = waveform / waveform.abs().max()
+
+        if waveform.shape[1] > self.fixed_length:
+            waveform = waveform[:, :self.fixed_length]
+        elif waveform.shape[1] < self.fixed_length:
+            pad_len = self.fixed_length - waveform.shape[1]
+            waveform = torch.nn.functional.pad(waveform, (0, pad_len))
+
+        return waveform
+    
+    def __getitem__(self, index):
+        row = self.dataset_df.iloc[index]
+        waveform1 = self._preprocess(row['audio_path_1'])
+        waveform2 = self._preprocess(row['audio_path_2'])
+        label = torch.tensor(row['label'], dtype=torch.float)
+
+        return waveform1, waveform2, label
     
                 
 
