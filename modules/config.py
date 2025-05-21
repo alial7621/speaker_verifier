37e5463ac0096541f11ca2251c7ce9d9f8c480cb
alial7621 <a_lashini@yahoo.com>
2025-05-22 00:17:31 +0330

    fix(config): fixing lines

--- modules/config.py
+++ modules/config.py
@@ -12,17 +12,17 @@ def get_argparser():
     train_parser.add_argument("--code_dir", type=str, default="./")
     train_parser.add_argument("--checkpoint_dir", type=str, default="./checkpoints")
     train_parser.add_argument("--dataset_dir", type=str, default="./dataset", 
-                        help="path to the dataset csv and text files")
+                              help="path to the dataset csv and text files")
     train_parser.add_argument("--data_dir", type=str, default="/dataset/data")
 
     # Data Preprocessing
     train_parser.add_argument("--sample_rate", type=int, default=16000, help="Audio sample rate")
     train_parser.add_argument("--duration", type=int, default=3,
-                        help="Input audio files must have same duration (unit: seconds)")
+                              help="Input audio files must have same duration (unit: seconds)")
     train_parser.add_argument("--no_vad", dest="vad", action="store_false", 
-                        help="Disable Voice Active Detection in preprocessing audio samples")
+                              help="Disable Voice Active Detection in preprocessing audio samples")
     train_parser.add_argument("--no_augmentation", dest="augmentations", action="store_false",
-                        help="Use Augmentation for audio samples during training")
+                              help="Use Augmentation for audio samples during training")
     
     # Model
     train_parser.add_argument("--in_channels", type=int, default=80, help="Input channel to the model")
@@ -31,35 +31,35 @@ def get_argparser():
     
     # Train
     train_parser.add_argument("--no_manual_seed", dest="manual_seed", action="store_false", 
-                        help="Whether use manual seed or not")
+                              help="Whether use manual seed or not")
     train_parser.add_argument("--seed", type=int, default=72322)
     train_parser.add_argument("--batch_size", type=int, default=8)
     train_parser.add_argument("--samples_per_epoch", type=int, default=30000,
-                        help="number of the training samples per epoch")
+                              help="number of the training samples per epoch")
     train_parser.add_argument("--epochs", type=int, default=50)
-    train_parser.add_argument("--load_checkpoint", type=str, default="./checkpoints/models/last_model.pt",
-                        help="Start from the last checkpoint. Always refer as last_model.pt")
+    train_parser.add_argument("--checkpoint_path", type=str, default="./checkpoints/models/last_model.pt",
+                              help="Start from the last checkpoint. Always refer as last_model.pt")
     
     # Optimizer and scheduler
     train_parser.add_argument("--loss_type", type=str, default="contrastive",
-                        help="Choose the loss function, possible inputs:[contrastive, triplet, cosineemb]")
+                              help="Choose the loss function, possible inputs:[contrastive, triplet, cosineemb]")
     train_parser.add_argument("--margin_loss", type=float, default=0.2,
-                        help="The margin, using in the loss function")
+                              help="The margin, using in the loss function")
     train_parser.add_argument("--eer_thresh", type=float, default=None, help="Threshold for EER metric")
     train_parser.add_argument("--lr", type=float, default=1e-2,
-                        help="Initial learning rate for scheduler")
+                              help="Initial learning rate for scheduler")
     train_parser.add_argument("--final_lr", type=float, default=1e-6,
-                        help="Final learning rate for scheduler")
+                              help="Final learning rate for scheduler")
     train_parser.add_argument("--weight_decay_factor", type=float, default=0.2,
-                        help="Weight decay factor for scheduler")
+                              help="Weight decay factor for scheduler")
     train_parser.add_argument("--lr_wait", type=int, default=3,
-                        help="Number of cicles without significant improvement in EER for scheduler")
+                              help="Number of cicles without significant improvement in EER for scheduler")
     train_parser.add_argument("--lr_thresh", type=float, default=1e-3,
-                        help="Threshold to check plateau-ing of loss")
+                              help="Threshold to check plateau-ing of loss")
     train_parser.add_argument("--momentum1", type=float, default=0.5,
-                        help="Optimizer momentum 1 value")
+                              help="Optimizer momentum 1 value")
     train_parser.add_argument("--momentum2", type=float, default=0.999,
-                        help="Optimizer momentum 1 value")
+                              help="Optimizer momentum 1 value")
     
     # Test parser
     test_parser = subparsers.add_parser("test", help="Test model")
