35687f92ea5dad72973aa95b77a96a4a3ed64b9e
alial7621 <a_lashini@yahoo.com>
2025-05-22 12:19:05 +0330

    feat(util): write plot_metrics function

---
+++ utils/util.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+
+def plot_metrics(metrics, save_path=None):
+    """
+    Plot training metrics.
+    
+    Args:
+        metrics (dict): Dictionary of metrics
+        save_path (str, optional): Path to save plot
+    """
+    # Create figure
+    fig, axes = plt.subplots(1, 2, figsize=(12, 8))
+    
+    # Plot training loss
+    if 'loss' in metrics:
+        axes[0, 0].plot(metrics['loss'])
+        axes[0, 0].set_title("Training Loss")
+        axes[0, 0].set_xlabel("Epoch")
+        axes[0, 0].set_ylabel("Loss")
+    
+    # Plot val eer
+    if 'eer' in metrics:
+        axes[0, 1].plot(metrics['eer'])
+        axes[0, 1].set_title("Val EER")
+        axes[0, 1].set_xlabel("Epoch")
+        axes[0, 1].set_ylabel("EER")
+    
+    # Save or show
+    plt.tight_layout()
+    if save_path:
+        plt.savefig(save_path)
+        plt.close()
+    else:
+        plt.show()
