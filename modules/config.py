cdbfb859aac64a30b2c0c85ec2bc91f2f4fdaf43
alial7621 <a_lashini@yahoo.com>
2025-05-24 00:36:42 +0330

    feat(config): add model arguments to testparser

--- modules/config.py
+++ modules/config.py
@@ -78,8 +78,14 @@ def get_argparser():
                              help="Disable Voice Active Detection in preprocessing audio samples")
     test_parser.add_argument("--batch_size", type=int, default=32)
     test_parser.add_argument("--eer_thresh", type=float, default=None, help="Threshold for EER metric")
+    test_parser.add_argument("--find_optim_thresh", action="store_true", help="Find the optimal threshold by validation data")
     test_parser.add_argument("--single_pred", action="store_true", help="Single input prediction")
     test_parser.add_argument("--audio1", type=str, default=None, help="Use in single prediction mode")
     test_parser.add_argument("--audio2", type=str, default=None, help="Use in single prediction mode")
+    
+    # Model
+    test_parser.add_argument("--mfcc_feat_dim", type=int, default=80, help="Input channel to the model")
+    test_parser.add_argument("--channels", type=int, default=256, help="Intermediate channels")
+    test_parser.add_argument("--embd_dim", type=int, default=192, help="Output embed size")
     
     return parser
