a42556dbb173db96d3ba115b580af3fc846a5fa5
alial7621 <a_lashini@yahoo.com>
2025-05-24 22:16:16 +0330

    fix(util): fix plotting diagrams

--- utils/util.py
+++ utils/util.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import torch
 
 
 def plot_metrics(metrics, save_path=None):
@@ -10,21 +11,20 @@ def plot_metrics(metrics, save_path=None):
         save_path (str, optional): Path to save plot
     """
     # Create figure
-    fig, axes = plt.subplots(1, 2, figsize=(12, 8))
-    
+    fig, axes = plt.subplots(1, 2, figsize=(15, 6))
     # Plot training loss
     if 'loss' in metrics:
-        axes[0, 0].plot(metrics['loss'])
-        axes[0, 0].set_title("Training Loss")
-        axes[0, 0].set_xlabel("Epoch")
-        axes[0, 0].set_ylabel("Loss")
+        axes[0].plot(metrics['loss'])
+        axes[0].set_title("Training Loss")
+        axes[0].set_xlabel("Epoch")
+        axes[0].set_ylabel("Loss")
     
     # Plot val eer
     if 'eer' in metrics:
-        axes[0, 1].plot(metrics['eer'])
-        axes[0, 1].set_title("Val EER")
-        axes[0, 1].set_xlabel("Epoch")
-        axes[0, 1].set_ylabel("EER")
+        axes[1].plot(metrics['eer'])
+        axes[1].set_title("Val EER")
+        axes[1].set_xlabel("Epoch")
+        axes[1].set_ylabel("EER")
     
     # Save or show
     plt.tight_layout()
